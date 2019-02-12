bb8b5d62930e869f25ddaba67ccf11f8cb820e27
Keith Dahlby <keith@newbo.co>
2019-02-11 22:34:46 -0600

    Initial map

--- js/viz.js
+++ js/viz.js
@@ -1,15 +1,23 @@
-/* globals d3, crossfilter, dc */
+/* globals d3, crossfilter, dc, topojson */
 
 var dims;
 var groups;
+var schoolData;
+var districtShapes;
 
-(function(d3, crossfilter, dc) {
+(function(d3, crossfilter, dc, topojson) {
   var charts = {
     gradeBands: dc.rowChart('#gradeBands'),
     csResponses: dc.pieChart('#csResponses'),
+    map: dc.geoChoroplethChart('#map'),
     table: dc.dataTable('#districts')
   };
 
+  d3.json('./data/districts.simple2.topo.json')
+    .then(function (shapes) {
+      districtShapes = shapes;
+      if (schoolData) render(schoolData, districtShapes);
+    });
   d3.csv('./data/gradeLevelData.csv', function (school) {
     var gradeBands = [];
 
@@ -30,17 +38,58 @@ var groups;
       gradeBands: gradeBands,
       csResponse: school['Teaches CS?'] || 'Unknown',
     };
-  }).then(render);
+  }).then(function (schools) {
+    schoolData = schools;
+    if (districtShapes) render(schoolData, districtShapes);
+  });
 
-  function render(schools) {
+  var good = '#00aeef'; // NewBoCo Blue
+  var bad = d3.hsl(good);
+  bad.l *= 2;
+  var color = d3.scaleLinear()
+    .domain([0,1])
+    .range([bad, good]);
+  function districtColor(schoolCount, responseCount, csYesCount) {
+    var res = d3.rgb(color(responseCount / schoolCount));
+    res.opacity = csYesCount ? (
+      csYesCount === schoolCount ? 1 : 0.5
+    ) : 0;
+    return res;
+  }
+
+  function render(schools, districts) {
     var ndx = crossfilter(schools);
     dims = {
+      districtMap: ndx.dimension(dc.pluck('district')),
       district: ndx.dimension(dc.pluck('district')),
       school: ndx.dimension(dc.pluck('school')),
       gradeBands: ndx.dimension(dc.pluck('gradeBands'), true),
       csResponses: ndx.dimension(dc.pluck('csResponse'))
     };
     groups = {
+      districtMap: dims.districtMap.group().reduce(
+        function reduceAdd(p, v) {
+          return v.population > 0 ? {
+            count: p.count + 1,
+            responses: p.responses + (v.csResponse === 'Unknown' ? 0 : 1),
+            cs: p.cs + (v.csResponse === 'Yes' ? 1 : 0)
+          } : p;
+        },
+        function reduceRemove(p, v) {
+          return v.population > 0 ? {
+            count: p.count - 1,
+            responses: p.responses - (v.csResponse === 'Unknown' ? 0 : 1),
+            cs: p.cs - (v.csResponse === 'Yes' ? 1 : 0)
+          } : p;
+        },
+        function reduceInit() {
+          return {
+            count: 0,
+            responses: 0,
+            cs: 0,
+          };
+        }
+      ),
       gradeBands: dims.gradeBands.group().reduceCount(),
       schools: (function() {
         var group = dims.district.group().reduce(
@@ -85,6 +134,30 @@ var groups;
       .ordering(function (d) { return csResponsesOrder.indexOf(d.key); })
     ;
 
+    var geojson = topojson.feature(districts, districts.objects.IowaSchoolDistrictsFY18);
+    var projection = d3.geoAlbersUsa()
+      .fitSize([990,500], geojson);
+
+    charts.map.width(990)
+      .height(500)
+      .dimension(dims.districtMap)
+      .group(groups.districtMap)
+      .colorCalculator(function (d) {
+        return d ? districtColor(d.count, d.responses, d.cs) : '#ccc';
+      })
+      .overlayGeoJson(geojson.features, 'SchoolName', function (d) {
+        return d.properties.SchoolName.toUpperCase();
+      })
+      .projection(projection)
+      .valueAccessor(function(kv) {
+        console.log(kv);
+        return kv.value;
+      })
+      .title(function (d) {
+        return d.key;
+      })
+    ;
+
     charts.table
       .size(Infinity)
       .dimension(groups.schools)
@@ -116,12 +189,6 @@ var groups;
       ])
       .order(d3.ascending.bind(null))
       .renderlet(function(chart){
-        var good = "#00aeef";
-        var bad = d3.hsl(good);
-        bad.l *= 2;
-        var color = d3.scaleLinear()
-          .domain([0,1])
-          .range([bad, good]);
         chart.selectAll('tr.dc-table-row')
           .style('background-color', function(g) { 
             if(!g.value.length)
@@ -135,17 +202,11 @@ var groups;
               return s.csResponse !== 'Unknown';
             });
 
-            var l = responded.length;
-            var o = yes.length;
-            var yesColor = d3.rgb(color(l / g.value.length));
-            yesColor.opacity = o ? (
-              o === g.value.length ? 1 : 0.5
-              ) : 0;
-            return yesColor;
-         })
+            return districtColor(g.value.length, responded.length, yes.length);
+          });
       })
     ;
 
     dc.renderAll();
   }
-})(d3, crossfilter, dc);
+})(d3, crossfilter, dc, topojson);
