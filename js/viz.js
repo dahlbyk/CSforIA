9f09bb23fed94fb6a3f143e710fbd444977465c8
Keith Dahlby <keith@newbo.co>
2019-02-11 22:34:46 -0600

    Initial map

--- js/viz.js
+++ js/viz.js
@@ -1,15 +1,23 @@
-/* globals d3, crossfilter, dc */
+/* globals d3, crossfilter, dc, topojson */
 
 var dims;
 var groups;
+var schoolData;
+var districtShapes;
 
-(function(d3, crossfilter, dc) {
+(function(d3, crossfilter, dc, topojson) {
   var charts = {
     gradeBands: dc.rowChart('#gradeBands'),
     csResponses: dc.pieChart('#csResponses'),
+    map: dc.geoChoroplethChart('#map'),
     table: dc.dataTable('#districts')
   };
 
+  d3.json('./data/districts.simple2.topo.json')
+    .then(function (shapes) {
+      districtShapes = shapes;
+      if (schoolData) render(schoolData, districtShapes);
+    });
   d3.csv('./data/gradeLevelData.csv', function (school) {
     var gradeBands = [];
 
@@ -30,17 +38,44 @@ var groups;
       gradeBands: gradeBands,
       csResponse: school['Teaches CS?'] || 'Unknown',
     };
-  }).then(render);
+  }).then(function (schools) {
+    schoolData = schools;
+    if (districtShapes) render(schoolData, districtShapes);
+  });
 
-  function render(schools) {
+  function render(schools, districts) {
     var ndx = crossfilter(schools);
     dims = {
+      districtMap: ndx.dimension(dc.pluck('district')),
       district: ndx.dimension(dc.pluck('district')),
       school: ndx.dimension(dc.pluck('school')),
       gradeBands: ndx.dimension(dc.pluck('gradeBands'), true),
       csResponses: ndx.dimension(dc.pluck('csResponse'))
     };
     groups = {
+      districtMap: dims.districtMap.group().reduce(
+        function reduceAdd(p, v) {
+          return v.population > 0 ? {
+            count: p.count + 1,
+            responses: p.responses + (v.csResponse === 'Unknown' ? 0 : 1),
+            cs: p.cs + (v.csResponse === 'Yes' ? 1 : 0)
+          } : p;
+        },
+        function reduceRemove(p, v) {
+          return v.population > 0 ? {
+            count: p.count - 1,
+            responses: p.responses - (v.csResponse === 'Unknown' ? 0 : 1),
+            cs: p.cs - (v.csResponse === 'Yes' ? 1 : 0)
+          } : p;
+        },
+        function reduceInit() {
+          return {
+            count: 0,
+            responses: 0,
+            cs: 0,
+          };
+        }
+      ),
       gradeBands: dims.gradeBands.group().reduceCount(),
       schools: (function() {
         var group = dims.district.group().reduce(
@@ -83,6 +118,32 @@ var groups;
       .dimension(dims.csResponses)
       .group(groups.csResponses)
       .ordering(function (d) { return csResponsesOrder.indexOf(d.key); })
+    ;
+
+    var geojson = topojson.feature(districts, districts.objects.IowaSchoolDistrictsFY18);
+    var projection = d3.geoAlbersUsa()
+      .fitSize([990,500], geojson);
+
+    charts.map.width(990)
+      .height(500)
+      .dimension(dims.districtMap)
+      .group(groups.districtMap)
+      .colors(d3.scaleQuantize().range(['#E2F2FF', '#C4E4FF', '#9ED2FF', '#81C5FF', '#6BBAFF', '#51AEFF', '#36A2FF', '#1E96FF', '#0089FF', '#0061B5']))
+      .colorDomain([0, 1])
+      .colorCalculator(function (d) {
+        return d ? charts.map.colors()(d.cs / d.count) : '#ccc';
+      })
+      .overlayGeoJson(geojson.features, 'SchoolName', function (d) {
+        return d.properties.SchoolName.toUpperCase();
+      })
+      .projection(projection)
+      .valueAccessor(function(kv) {
+        console.log(kv);
+        return kv.value;
+      })
+      .title(function (d) {
+        return d.key;
+      })
     ;
 
     charts.table
@@ -128,4 +189,4 @@ var groups;
 
     dc.renderAll();
   }
-})(d3, crossfilter, dc);
+})(d3, crossfilter, dc, topojson);
